759c5b5b5e47d8ee302bd36b1c6b8c8e405c3794
LoginamNet <loginamnet@gmail.com>
2023-03-13 19:07:10 +0300

    init: add configuried webpack

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const x = 0;
+
+  return (
+    <div className="App">
+      <h1 className="header">Hello World! {x}</h1>
+    </div>
+  );
+}
+
+export default App;
