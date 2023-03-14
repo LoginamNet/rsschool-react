72bce7d1da645062df21bd5f27f9beb4ea924d9e
LoginamNet <loginamnet@gmail.com>
2023-03-14 16:15:16 +0300

    feat: add tests for about and 404 pages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
-import { Search } from 'components/search/Search';
 import ReactDOM from 'react-dom';
+import { About } from 'pages/About';
+import { NotFoundPage } from 'pages/NotFound';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
-test("should render root without crashing", () => {
+test('should render root without crashing', () => {
   waitFor(() => {
-    const div = document.createElement("div");
-    div.id = "root";
+    const div = document.createElement('div');
+    div.id = 'root';
     document.body.appendChild(div);
-    require("./index");
+    require('./index');
     expect(ReactDOM.render).toHaveBeenCalledWith(<App />, div);
   })
 });
 
 test('should render app without crashing', () => {
   render(<App />);
+
   const homeLink = screen.getByText(/Home/);
   const aboutLink = screen.getByText(/About us/);
   expect(homeLink).toBeInTheDocument();
   expect(aboutLink).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
-  render(<Search />);
+test('should render about page', () => {
+  render(<About />);
+
+  const header = screen.getByRole('heading');
+  expect(header).toBeInTheDocument();
+  expect(header).toHaveTextContent('Some info about us!');
+});
+
+test('should render 404 page', () => {
+  
+  render(
+    <Router>
+      <NotFoundPage />
+    </Router>
+  );
+
+  const header = screen.getByRole('heading');
+  expect(header).toBeInTheDocument();
+  expect(header).toHaveTextContent('Oops!');
 });
