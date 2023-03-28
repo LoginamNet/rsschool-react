596c06b5f6e124e85da8381fc825b442d8447ea6
LoginamNet <loginamnet@gmail.com>
2023-03-28 08:25:35 +0300

    refactor: refactor single form card to function

--- src/components/form/FormCard.tsx
+++ src/components/form/FormCard.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 
 import { FormCard as ComponentProps } from 'pages/Form';
 
-export class SingleFormCard extends React.Component<ComponentProps> {
-  constructor(props: ComponentProps) {
-    super(props);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="formCardContainer" role="formcard">
-        <div className="formCardImage" style={{ backgroundImage: `url(${this.props.file})` }}></div>
-        <span className="formCardDateHand">
-          {this.props.date} / {this.props.radio.toUpperCase() + ' HAND'}
-        </span>
-        <div className="formCardDefinition">
-          <li className="formCardWater">{this.props.checked ? 'Waterproof' : 'Not waterproof'}</li>
-          <li className="formCardMaterial">{this.props.selected}</li>
-          <span className="formCardDescription">{this.props.text}</span>
-        </div>
-        <span className="formCardName">{this.props.name}</span>
+export function SingleFormCard(props: ComponentProps) {
+  return (
+    <div className="formCardContainer" role="formcard">
+      <div className="formCardImage" style={{ backgroundImage: `url(${props.file})` }}></div>
+      <span className="formCardDateHand">
+        {props.date} / {props.radio.toUpperCase() + ' HAND'}
+      </span>
+      <div className="formCardDefinition">
+        <li className="formCardWater">{props.checked ? 'Waterproof' : 'Not waterproof'}</li>
+        <li className="formCardMaterial">{props.selected}</li>
+        <span className="formCardDescription">{props.text}</span>
       </div>
-    );
-  }
+      <span className="formCardName">{props.name}</span>
+    </div>
+  );
 }
