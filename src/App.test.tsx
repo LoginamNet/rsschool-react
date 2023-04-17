a3c1a30d090b97c8a9932d477ddbd94fa5174815
LoginamNet <loginamnet@gmail.com>
2023-04-17 23:43:44 +0300

    fix: update node to fix vitest warnings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
-
-// describe('Common tests', function () {
-//   test('should render root without crashing', () => {
-//     waitFor(() => {
-//       const div = document.createElement('div');
-//       div.id = 'root';
-//       document.body.appendChild(div);
-//       require('./index');
-//       expect(ReactDOM.render).toHaveBeenCalledWith(div);
-//     });
-//   });
-// });
 
 describe('App elements tests', function () {
   test('should render app without crashing', async () => {
