da3c65368549ec8f81a4f16c7b0c1ae1b3c87a4b
LoginamNet <loginamnet@gmail.com>
2023-04-18 14:32:35 +0300

    refactor: refactor server to render other pages

--- server.js
+++ server.js
@@ -6,61 +6,55 @@ import { createServer } from 'vite';
 
 // Constants
 const port = process.env.PORT || 5173;
-const base = process.env.BASE || '/';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-async function server() {
-  // Create http server
-  const app = express();
+// Create http server
+const app = express();
 
-  // Add Vite or respective production middlewares
-  const vite = await createServer({
-    server: { middlewareMode: true },
-    appType: 'custom',
-    base,
-  });
-  app.use(vite.middlewares);
+// Add Vite or respective production middlewares
+const vite = await createServer({
+  server: { middlewareMode: true },
+  appType: 'custom',
+});
+app.use(vite.middlewares);
 
-  // Serve HTML
-  app.use('*', async (req, res) => {
-    const url = req.originalUrl.replace(base, '');
+// Serve HTML
+app.use('*', async (req, res) => {
+  const url = req.originalUrl;
 
-    try {
-      let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
-      template = await vite.transformIndexHtml(url, template);
-      const html = template.split(`<!--ssr-outlet-->`);
-      const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
+  try {
+    let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+    template = await vite.transformIndexHtml(url, template);
+    const html = template.split('<!--ssr-app-->');
+    const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
 
-      const { pipe } = await render(url, {
-        onShellReady() {
-          res.write(html[0]);
-          pipe(res);
-        },
-        onShellError() {
-          res.statusCode = 500;
-          res.setHeader('content-type', 'text/html');
-          res.send('<h1>Something went wrong</h1>');
-        },
-        onAllReady() {
-          res.write(html[0] + html[1]);
-          res.end();
-        },
-        onError(err) {
-          console.error(err);
-          logServerCrashReport(err);
-        },
-      });
-    } catch (err) {
-      vite.ssrFixStacktrace(err);
-      console.log(err.stack);
-      res.status(500).end(err.stack);
-    }
-  });
+    const { pipe } = await render(url, {
+      onShellReady() {
+        res.write(html[0]);
+        pipe(res);
+      },
+      onShellError() {
+        res.statusCode = 500;
+        res.setHeader('content-type', 'text/html');
+        res.send('<h1>Something went wrong</h1>');
+      },
+      onAllReady() {
+        res.write(html[0] + html[1]);
+        res.end();
+      },
+      onError(err) {
+        console.error(err);
+        logServerCrashReport(err);
+      },
+    });
+  } catch (err) {
+    vite.ssrFixStacktrace(err);
+    console.log(err.stack);
+    res.status(500).end(err.stack);
+  }
+});
 
-  // Start http server
-  app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-  });
-}
-
-server();
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+
 import { Layout } from 'components/layout/Layout';
 import { Main } from 'pages/Main';
 import { About } from 'pages/About';
