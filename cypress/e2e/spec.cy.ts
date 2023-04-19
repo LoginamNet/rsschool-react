af56a15606a1cc040f8b1da9f72ce44571f61fa0
LoginamNet <loginamnet@gmail.com>
2023-04-19 12:08:39 +0300

    feat: add some e2e pages tests

--- cypress/e2e/spec.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-  beforeEach(() => {
-    cy.visit('/about');
-  });
-  it('Does not do much!', () => {
-    cy.wait(500);
-    cy.get('.headerTitle').contains('ABOUT US');
-  });
-});
