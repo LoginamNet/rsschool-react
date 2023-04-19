f732d4f692460186d8826376dee92437570f42ed
LoginamNet <loginamnet@gmail.com>
2023-04-19 23:53:47 +0300

    feat: add search e2e tests

---
+++ cypress/e2e/Search.cy.ts
@@ -0,0 +1,33 @@
+describe('Search test', () => {
+  it('Should update search field on input', () => {
+    cy.visit('/');
+    cy.wait(500);
+
+    cy.get('.searchInput').type('Cat');
+    cy.get('.searchInput').should('have.value', 'Cat');
+  });
+
+  it('Should update search field and find on enter press', () => {
+    cy.visit('/');
+    cy.wait(500);
+
+    cy.get('.searchInput').type('Cat');
+    cy.get('.searchInput').type('{enter}');
+    cy.get('.searchInput').should('have.value', 'Cat');
+    cy.get('.CardContainer').should('have.length.of.at.most', 15);
+  });
+
+  it('Should update search field and find on find button press', () => {
+    cy.visit('/');
+    cy.wait(500);
+
+    cy.get('.searchInput').type('Dog');
+    cy.get('.searchButton').click();
+    cy.get('.searchInput').should('have.value', 'Dog');
+    cy.get('.CardContainer').should('have.length.of.at.most', 15);
+  });
+
+  it('Just a test to remove page load on coverage saving', () => {
+    expect(true).to.equal(true);
+  });
+});
